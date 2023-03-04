3a3d09b31bdc8962b82c622ee3dec929e55ad32d
Hyper-ActiveX <jayactive11112@gmail.com>
2023-03-04 19:40:10 +0530

    authentication pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import * as React from "react";
 
-function App() {
+import EnterEmail from "./components/pages/enterEmail";
+//import { Button, } from '@mui/material';
+
+
+// import DashBoard from "./components/pages/dashBoard";
+// import PrimarySearchAppBar from "./components/pages/navbar";
+import ForgetPassword from "./components/pages/resetPassword";
+import SignIn from "./components/pages/signIn";
+import SignUp from "./components/pages/signUp";
+
+const App = () => {
   return (
-    <div className="App">
-     <h1>Price Tracker Loading.....</h1>
-    </div>
+    // <div className="App">
+    //     <Button variant="contained">Hello World</Button>
+    // </div>
+    <>
+      {/* <PrimarySearchAppBar/> */}
+        {/* <DashBoard/> */}
+        <SignIn/>
+        <SignUp/>
+        <ForgetPassword/>
+        <EnterEmail/>
+    </>
+
   );
 }
 
---
+++ client/src/components/pages/resetPassword.jsx
@@ -0,0 +1,119 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { InputAdornment } from '@mui/material';
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function ResetPassword() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      password: data.get('password'),
+    });
+  };
+
+  const email={
+    id:"user123@email.com"
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box sx={{marginTop: 10,display: 'flex',flexDirection: 'column',alignItems: 'center',}}>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Reset Password
+          </Typography>
+        <TextField
+            sx={{border:2,borderRadius:10, height:'min-content'}}
+            id="emailId"
+            value={email.id}
+            readOnly
+            InputProps={{
+            startAdornment: (
+                <InputAdornment position="start">
+                <AccountCircle />
+                </InputAdornment>
+            ),
+            }}
+             variant="standard"
+        />
+        
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="Password"
+              label="New Password"
+              name="password"
+              autoComplete="password"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Confirm Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Sign in"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+      <Typography paddingTop={4}><Copyright/></Typography>
+    </ThemeProvider>
+  );
+}
