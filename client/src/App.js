f0a3842e52021f69a3e3f3875c169f8596a41b0e
Kanha Tiwari <tiwarikanha26@gmail.com>
2023-03-06 15:03:08 +0530

    changed the alignement of homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,36 @@
 import React from "react";
 import AllProducts from "./components/AllProducts";
 import IntroCard from "./components/IntroCard";
+import { Container } from "@mui/system";
 import ProductCount from "./components/ProductCount";
 import SideBar from "./components/SideBar";
+import "./styles/style.css";
 
 function App() {
   return (
     <div>
       <SideBar />
-      <div style={{ marginLeft: "220px" }}>
-        <IntroCard />
-        <div style={{ marginTop: "10px" }}>
-          <ProductCount />
-        </div>
-        <AllProducts />
+      <div style={{ marginLeft: "100px" }}>
+        <Container
+          maxWidth="sm"
+          style={{
+            marginLeft: "0px",
+            // padding: "0",
+            marginTop: "10px",
+            height: "95vh",
+            // border: "solid 2px black",
+            display: "flex",
+            flexDirection: "column",
+            // paddingRight: "0px",
+            justifyContent: "space-between",
+            gap: "10px",
+            padding: "0px 0px",
+          }}
+        >
+          <IntroCard />
+          <AllProducts />
+        </Container>
+        {/* <ProductCount /> */}
       </div>
     </div>
   );
--- client/src/components/IntroCard.jsx
+++ client/src/components/IntroCard.jsx
@@ -5,34 +5,41 @@ import Box from "@mui/material/Box";
 
 const IntroCard = () => {
   return (
-    <Container
-      sx={{
-        padding: "20px",
-        maxWidth: "sx",
-        backgroundColor: "#F5F5F7",
-        borderRadius: "14px",
-      }}
-    >
-      <Box sx={{ display: "flex" }}>
-        <Box sx={{ width: "70%", p: 2, textAlign: "left" }}>
-          <h1>Hello There!</h1>
-          <b>Its good to see you again</b>
+    <Container>
+      <Container
+        // maxWidth="sm"
+        sx={{
+          marginLeft: "0px",
+          marginRight: "0px",
+          width: "100%",
+          backgroundColor: "#F5F5F7",
+          borderRadius: "14px",
+          // border: "solid 2px black",
+          padding: "15px 0px",
+          paddingRight: "0px",
+        }}
+      >
+        <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+          <Box sx={{ flex: "7", p: 2, textAlign: "left" }}>
+            <h1>Hello There!</h1>
+            <b>Its good to see you again</b>
+          </Box>
+          <Box
+            sx={{
+              flex: "3",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Avatar
+              alt="Name"
+              src="/static/images/avatar/1.jpg"
+              sx={{ width: 56, height: 56 }}
+            />
+          </Box>
         </Box>
-        <Box
-          sx={{
-            width: "30%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Avatar
-            alt="Name"
-            src="/static/images/avatar/1.jpg"
-            sx={{ width: 56, height: 56 }}
-          />
-        </Box>
-      </Box>
+      </Container>
     </Container>
   );
 };
