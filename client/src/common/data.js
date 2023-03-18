e4f7a071b11a6cc5f5d20566979feb52de70c7bd
Kanha Tiwari <tiwarikanha26@gmail.com>
2023-03-18 18:09:29 +0530

    added dummy chart for product price changes

--- client/src/common/data.js
+++ client/src/common/data.js
@@ -1,1 +1,38 @@
 export const supportedWebsites = ["flipkart", "amazon"];
+
+export const chartDummyData = [
+  {
+    id: "I phone 14 Pro",
+    color: "hsl(123, 70%, 50%)",
+    data: [
+      {
+        y: 40,
+        x: "22-03-2012",
+      },
+      {
+        y: 35,
+        x: "26-03-2012",
+      },
+      {
+        y: 45,
+        x: "29-03-2012",
+      },
+      {
+        y: 41,
+        x: "31-03-2012",
+      },
+      {
+        y: 37,
+        x: "11-04-2012",
+      },
+      {
+        y: 43,
+        x: "24-04-2012",
+      },
+      {
+        y: 47,
+        x: "20-04-2012",
+      },
+    ],
+  },
+];
