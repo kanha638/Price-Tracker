8062adfb0c7f02127c205420d1eb374a9a7bc144
Kanha Tiwari <tiwarikanha26@gmail.com>
2023-05-22 11:42:27 +0530

    new ui configuration completed and added loader inside the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,7 @@
 import React, { useEffect } from "react";
-import "./styles/style.css";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
-import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ResetPassword from "./components/ResetPassword";
 import { Me } from "./middleware/auth";
 import { useDispatch } from "react-redux";
-import { ThemeProvider } from "@emotion/react";
-import { globalTheme } from "../src/utils/themes";
 
 function App() {
   const dispatch = useDispatch();
@@ -19,20 +12,7 @@ function App() {
     CallMeRoute();
   }, []);
 
-  return (
-    <div>
-      <ThemeProvider theme={globalTheme}>
-        <Router>
-          <Routes>
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/*" element={<Home />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </div>
-  );
+  return <div>Hello from frontene</div>;
 }
 
 export default App;
