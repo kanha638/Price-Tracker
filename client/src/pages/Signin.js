363811145a1265e904c91842b2a4c0165f3e4d4a
ChahitKolte <chahitkolte@gmail.com>
2023-05-22 23:46:21 +0530

    signin and signup pages

---
+++ client/src/pages/Signin.js
@@ -0,0 +1,122 @@
+import { React, useState } from "react";
+import { TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
+import { Box } from "@mui/material";
+import logo_pt from "../assets/images/favicon.jpeg";
+const Signin = () => {
+  const [details, setDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  function changeHandler(e) {
+    setDetails({ ...details, [e.target.id]: [e.target.value] });
+  }
+  return (
+    <div
+      style={{
+        backgroundColor: "#f8f4f3",
+        minHeight: "100vh",
+        margin: "0px",
+        padding: "0px",
+      }}
+    >
+      <form>
+        <Box
+          display="flex"
+          flexDirection={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          marign="auto"
+          // marginTop={5}
+          padding={2}
+          borderRadius={2}
+          sx={{
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundColor: "white",
+            minWidth: "300px",
+            maxWidth: "550px",
+            width: "90%",
+            height: "620px",
+            position: "absolute",
+            top: "45%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.08)",
+          }}
+        >
+          <img
+            alt="logo"
+            src={logo_pt}
+            style={{ height: "120px", width: "120px", borderRadius: "50%" }}
+          />
+          <Typography
+            variant="h4"
+            style={{ fontSize: "40px", marginTop: "20px" }}
+            padding={0}
+            textAlign="center"
+          >
+            Login
+          </Typography>
+
+          <TextField
+            variant={"outlined"}
+            margin="normal"
+            placeholder="Email / mobile-number"
+            type={"email"}
+            autoComplete="email"
+            sx={{ width: "90%" }}
+          />
+          <TextField
+            // id="outlined-password-input"
+            variant={"outlined"}
+            placeholder="Password"
+            type={"password"}
+            margin="normal"
+            sx={{ width: "90%" }}
+          />
+          <div style={{ width: "90%" }}>
+            <Button
+              sx={{
+                marginTop: 3,
+                width: "100%",
+                height: "50px",
+              }}
+              style={{
+                borderRadius: 3,
+                backgroundColor: "black",
+                padding: "10px 10px",
+                fontSize: "20px",
+              }}
+              variant="contained"
+            >
+              Login
+            </Button>
+            <Grid container>
+              <Grid
+                item
+                sx={{
+                  display: "flex",
+                  width: "100%",
+                  marginTop: "20px",
+                  justifyContent: "space-between",
+                }}
+              >
+                <p>Don't have an account ?</p>
+                <Link to="/sign-up" className="link-redirect">
+                  Sign Up
+                </Link>
+              </Grid>
+            </Grid>
+          </div>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default Signin;
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,178 @@
+import React from "react";
+import { TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
+import { Box } from "@mui/material";
+import { useState } from "react";
+import logo_pt from "../assets/images/favicon.jpeg";
+
+const SignUp = () => {
+  const [details, setDetails] = useState({
+    name: "",
+    email: "",
+    mobileNum: "",
+    password: "",
+    confirmPassword: "",
+  });
+  function changeHandler(e) {
+    setDetails({ ...details, [e.target.id]: [e.target.value] });
+  }
+  return (
+    <div
+      style={{
+        backgroundColor: "#f8f4f3",
+        minHeight: "100vh",
+        margin: "0px",
+        padding: "0px",
+      }}
+    >
+      <form>
+        <Box
+          display="flex"
+          flexDirection={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          marign="auto"
+          // marginTop={5}
+          padding={2}
+          borderRadius={2}
+          sx={{
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundColor: "white",
+            minWidth: "300px",
+            maxWidth: "550px",
+            width: "90%",
+            height: "750px",
+            position: "absolute",
+            top: "45%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.08)",
+          }}
+        >
+          <img
+            alt="logo"
+            src={logo_pt}
+            style={{ height: "120px", width: "120px", borderRadius: "50%" }}
+          />
+
+          <Typography
+            variant="h4"
+            style={{ fontSize: "40px", marginTop: "20px" }}
+            padding={0}
+            textAlign="center"
+          >
+            Sign-Up
+          </Typography>
+          <TextField
+            margin="normal"
+            id="name"
+            placeholder="Your name"
+            name="name"
+            type={"text"}
+            value={details.name}
+            onChange={changeHandler}
+            autoComplete="name"
+            autoFocus
+            sx={{ width: "90%" }}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            value={details.email}
+            placeholder="Email"
+            name="email"
+            autoComplete="email"
+            onChange={changeHandler}
+            autoFocus
+            sx={{ width: "90%" }}
+          />
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="mobileNum"
+            placeholder="Mobile number"
+            name="mobileNum"
+            type={"text"}
+            value={details.mobileNum}
+            // error={details.mobileNum.length !== 10 ? true : false}
+            onChange={changeHandler}
+            autoComplete="mobileNum"
+            sx={{ width: "90%" }}
+          />
+
+          <TextField
+            margin="normal"
+            id="password"
+            name="password"
+            placeholder="Password"
+            type="password"
+            value={details.password}
+            onChange={changeHandler}
+            autoComplete="password"
+            sx={{ width: "90%" }}
+          />
+
+          <TextField
+            margin="normal"
+            name="confirmPassword"
+            placeholder="Confirm password"
+            value={details.confirmPassword}
+            error={
+              details.confirmPassword.length > 0 &&
+              details.confirmPassword !== details.password
+                ? true
+                : false
+            }
+            onChange={changeHandler}
+            type="password"
+            sx={{ width: "90%" }}
+          />
+          <div style={{ width: "90%" }}>
+            <Button
+              sx={{
+                marginTop: 3,
+                width: "100%",
+                height: "50px",
+              }}
+              style={{
+                borderRadius: 3,
+                backgroundColor: "black",
+                padding: "10px 10px",
+                fontSize: "20px",
+              }}
+              variant="contained"
+            >
+              SignUp
+            </Button>
+            <Grid container>
+              <Grid
+                item
+                sx={{
+                  display: "flex",
+                  width: "100%",
+                  marginTop: "20px",
+                  justifyContent: "space-between",
+                }}
+              >
+                <p>Already have an account ? </p>
+                <Link to="/sign-in" className="link-redirect">
+                  Sign In
+                </Link>
+              </Grid>
+            </Grid>
+          </div>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
