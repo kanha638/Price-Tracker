2d1471288c7b59169e67e794dca352cd624eb4a9
Kanha Tiwari <tiwarikanha26@gmail.com>
2023-05-25 20:50:58 +0530

    sliding issue in headerbox is resolved

--- client/src/components/headSlider/HeadSliderCard.jsx
+++ client/src/components/headSlider/HeadSliderCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -46,13 +46,24 @@ const HeadSliderCard = () => {
       },
     ],
   };
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const dataArray = [
+    "https://m.media-amazon.com/images/I/41b3PmlPPuL._SX300_SY300_QL70_FMwebp_.jpg",
+    "https://m.media-amazon.com/images/I/71hDrHwTsVL._SX679_PIbundle-6,TopRight,0,0_AA679SH20_.jpg",
+    "https://m.media-amazon.com/images/I/61S9aVnRZDL._SX679_.jpg",
+  ];
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % dataArray.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
       <Slider {...settings}>
-        <HeaderBox img="https://m.media-amazon.com/images/I/41b3PmlPPuL._SX300_SY300_QL70_FMwebp_.jpg" />
-        <HeaderBox img="https://m.media-amazon.com/images/I/71hDrHwTsVL._SX679_PIbundle-6,TopRight,0,0_AA679SH20_.jpg" />
-        <HeaderBox img="https://m.media-amazon.com/images/I/61S9aVnRZDL._SX679_.jpg" />
+        <HeaderBox img={dataArray[currentIndex]} />
       </Slider>
     </>
   );
