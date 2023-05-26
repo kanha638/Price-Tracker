df79363280eaae0424f74bc91986d1c68a2d054b
kamal kishor <ikkamalkish0r@gmail.com>
2023-05-26 14:38:28 +0530

    fixed doc string in add_product route

--- servers/scrapping-server/app/product/routes.py
+++ servers/scrapping-server/app/product/routes.py
@@ -10,10 +10,18 @@ def add_product():
     """
     This function is a Flask route that takes a POST request with a product URL in the body, scrapes the
     product details using an asyncio scraper, and returns a JSON object with the product information.
-    :return: The function `add_product()` returns a JSON object containing the details of a product
-    fetched from a given URL. The details include the product title, price, currency, MRP, availability,
-    rating, rating count, and image link. If the request method is not POST, the function returns the
-    string 'Invalid request.'
+
+    Method: POST
+    Body: {
+        "url" : ""
+    }
+
+    :return: 
+        A JSON object containing the details of a product
+        fetched from a given URL. The details include the product title, price, currency, MRP, availability, category,
+        rating, rating count, and image link. 
+        If the request method is not POST, the function returns the
+        string 'Invalid request.'
     """
 
     if request.method == 'POST':
