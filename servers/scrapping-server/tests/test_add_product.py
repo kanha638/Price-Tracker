4a116e4dd3ad7639c18cdf647ebc2c594dd56b62
kamal kishor <ikkamalkish0r@gmail.com>
2023-05-30 23:28:33 +0530

    Reduced test urls

--- servers/scrapping-server/tests/test_add_product.py
+++ servers/scrapping-server/tests/test_add_product.py
@@ -5,19 +5,8 @@ import requests
 app_url = 'http://127.0.0.1:5000/product/register'
 
 
-def validate_product(product_details, url):
-    assert product_details['Title'] is not None, f"Title : {product_details['Title']} is None for URL: {url}"
-    assert product_details['Price'] is not None, f"Price is None for URL: {url}"
-    assert product_details['MRP'] is not None, f"MRP is None for URL: {url}"
-    assert product_details['Currency'] is not None, f"Currency is None for URL: {url}"
-    assert product_details['Category'] is not None, f"Category is None for URL: {url}"
-    assert product_details[
-        'Image_Link'] is not None, f"Image_Link is None for URL: {url}"
-    assert product_details['Website'] is not None, f"Website is None for URL: {url}"
-
-
 @pytest.mark.skip(reason="Not required for this run")
-# @pytest.mark.fail
+@pytest.mark.fail
 def test_add_product():
     # test when request body is not correct
     payload = {
