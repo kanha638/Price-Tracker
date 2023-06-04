fad0b39098e19c09f23471517e42b4fc0c4924a6
Kanha Tiwari <tiwarikanha26@gmail.com>
2023-06-04 20:18:14 +0530

    import issue in subscribed product page

--- client/src/components/subscribedProducts/SubscribedProducts.jsx
+++ client/src/components/subscribedProducts/SubscribedProducts.jsx
@@ -1,25 +1,23 @@
-import React,{useState,useEffect} from "react";
-import { useSelector,useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Data from "../../assets/dummyData";
-import Shop from "../shops/Shop";
+import Shop, { ShopSkel } from "../shops/Shop";
 import { fetchSubscribedProducts } from "../../middleware/product";
 import { UserState } from "../../slices/userSlice";
 
 const SubscribedProducts = () => {
-
   const userState = useSelector(UserState);
   const dispatch = useDispatch();
-  const [subscribedProduct,setSubscribedProduct] = useState([]);
-  useEffect(()=>{
-    
-    const getSubscribedProducts = async ()=>{
-      await fetchSubscribedProducts(dispatch,setSubscribedProduct)
+  const [subscribedProduct, setSubscribedProduct] = useState([]);
+  useEffect(() => {
+    const getSubscribedProducts = async () => {
+      await fetchSubscribedProducts(dispatch, setSubscribedProduct);
     };
     getSubscribedProducts();
   }, []);
   return (
     <>
-    {userState?.subscribedProductfetchStatusPending === true ? (
+      {userState?.subscribedProductfetchStatusPending === true ? (
         <ShopSkel title={"Subscribed Products"} />
       ) : (
         <Shop productItems={subscribedProduct} title={"Subscribed Products"} />
